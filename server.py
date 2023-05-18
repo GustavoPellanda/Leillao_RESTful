314287df45db0539514b2885cd43c3d39214f19e
Gustavo Finau Pellanda <129123498+GustavoPellanda@users.noreply.github.com>
2023-05-18 02:11:43 -0300

    lances funcionando

--- server.py
+++ server.py
@@ -9,14 +9,10 @@ class Servidor_Leilao(object):
         self.lances = {}
         self.thread_verificacao = None
     
-    def start_verification_thread(self):
+    def inicia_thread_esgotar(self):
         if self.thread_verificacao is None or not self.thread_verificacao.is_alive():
             self.thread_verificacao = threading.Thread(target=self.esgotar_leiloes)
             self.thread_verificacao.start()
-
-    def registrar_cliente(self, nome_cliente, referenciaCliente):
-        self.clientes[referenciaCliente] = nome_cliente
-        print(f"Novo cliente registrado: {nome_cliente}")
 
     def registrar_produto(self, codigo, nome, descricao, preco_inicial, tempo_final, nome_cliente):
         # Calcula o tempo limite do leilão
@@ -62,10 +58,12 @@ class Servidor_Leilao(object):
         # Atualiza o preço atual do produto:
         for produto in self.produtos:
             if produto["codigo"] == codigo:
+                print("aaaaaaaaaaaa passou aqui aaaaaaaaaaaa")
                 produto["preco_atual"] = lance
                 break
 
         print(f"Lance de {nome_cliente} registrado no produto {codigo} com valor R${lance:.2f}")
+        print(self.produtos)
         return True
 
     # Calcula o tempo restante dos leilões:
@@ -77,7 +75,6 @@ class Servidor_Leilao(object):
                 if tempo_restante <= 0:
                     # Deleta:
                     codigo = produto['codigo']
-                    nome = produto['nome']
                     self.lances.pop(codigo, None)
                     self.produtos.remove(produto)
                     print(f"Lances do produto {codigo} expirados.")
@@ -92,26 +89,18 @@ servidor = Servidor_Leilao()
 def index():
     return render_template('index.html')
 
-@app.route('/clientes', methods=['POST'])
-def registrar_cliente():
-    data = request.get_json()
-    nome_cliente = data['nome_cliente']
-    referencia_cliente = data['referencia_cliente']
-    servidor.registrar_cliente(nome_cliente, referencia_cliente)
-    return jsonify({'message': 'Cliente registrado com sucesso'})
-
 @app.route('/produtos', methods=['POST'])
 def registrar_produto():
     data = request.get_json()
-    codigo = data['codigo']
+    codigo = int(data['codigo'])
     nome = data['nome']
     descricao = data['descricao']
     preco_inicial = float(data['preco_inicial'])
     tempo_final = float(data['tempo_final'])
     nome_cliente = data['nome_cliente']
     servidor.registrar_produto(codigo, nome, descricao, preco_inicial, tempo_final, nome_cliente)
-    servidor.start_verification_thread()
-    return jsonify({'message': 'Produto registrado com sucesso'})
+    servidor.inicia_thread_esgotar()
+    return jsonify({'verificação': 'Produto registrado com sucesso'})
 
 @app.route('/produtos', methods=['GET'])
 def obter_produtos():
@@ -121,11 +110,11 @@ def obter_produtos():
 @app.route('/lances', methods=['POST'])
 def fazer_lance():
     data = request.get_json()
-    codigo = data['codigo']
-    lance = data['lance']
+    codigo = int(data['codigo'])
+    lance = float(data['lance'])
     nome_cliente = data['nome_cliente']
-    sucesso = servidor.fazer_lance(codigo, lance, nome_cliente)
-    return jsonify({'success': sucesso})
+    resposta = servidor.fazer_lance(codigo, lance, nome_cliente)
+    return jsonify({'verificação': 'lance = ' + str(resposta)})
     
 if __name__ == '__main__':
     app.run(debug=True)
